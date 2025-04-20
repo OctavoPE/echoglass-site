6b60aab87d050fb9c3cda49a433c06737bb9558a
OctavoPE <Jallain99@outlook.com>
2025-04-20 13:21:12 -0600

    final

--- app/tutorial/page.tsx
+++ app/tutorial/page.tsx
@@ -170,7 +170,7 @@ export default function TutorialPage() {
       <footer className="w-full border-t py-6">
         <div className="container flex flex-col items-center justify-center gap-4 md:flex-row md:gap-8">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            EchoGlass - A Project by The University of Utah
+            EchoGlass - The University of Utah
           </p>
         </div>
       </footer>
